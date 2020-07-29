74ddbb585d25a42a529f675f063870c62dc89748
Pip Rees <piprees@outlook.com>
2020-07-29 02:59:57 +0100

    fix: adding title

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,15 @@ import Introduction from '../components/Introduction.jsx'
 import CareerSummary from '../components/CareerSummary.jsx'
 import KeyKnowledgeAreas from '../components/KeyKnowledgeAreas.jsx'
 import GetInTouch from '../components/GetInTouch.jsx'
+import Head from 'next/head'
 
 export default function Index() {
   return (
     <>
+      <Head>
+        <title>Pip Rees | UI Designer & Front-End Developer</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Introduction />
       <CareerSummary />
       <KeyKnowledgeAreas />
