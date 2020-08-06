789c46afdcc2d40c5bb73d9c24d8058a86e41558
Pip Rees <piprees@outlook.com>
2020-08-06 01:03:22 +0100

    wip: removing comments to allow for testing

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Animation from '../sections/Animation.jsx'
 import Introduction from '../sections/Introduction.jsx'
 import CareerSummary from '../sections/CareerSummary.jsx'
 import KeyKnowledgeAreas from '../sections/KeyKnowledgeAreas.jsx'
-// import GraphicalWorks from '../sections/GraphicalWorks.jsx'
-// import Projects from '../sections/Projects.jsx'
-// import Articles from '../sections/Articles.jsx'
+import GraphicalWorks from '../sections/GraphicalWorks.jsx'
+import Projects from '../sections/Projects.jsx'
+import Articles from '../sections/Articles.jsx'
 import GetInTouch from '../sections/GetInTouch.jsx'
 import Footer from '../components/Footer.jsx'
 
@@ -19,9 +19,9 @@ export default function Index() {
       <Introduction />
       <CareerSummary />
       <KeyKnowledgeAreas />
-      {/* <Projects /> */}
-      {/* <GraphicalWorks /> */}
-      {/* <Articles /> */}
+      <Projects />
+      <GraphicalWorks />
+      <Articles />
       <GetInTouch />
       <Footer />
     </main>
